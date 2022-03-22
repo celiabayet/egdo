87b5ad870bc812d2d314b514bfe130b67acfd6ee
Jamal Sebbari <sebbarija@gmail.com>
2022-03-22 20:05:54 +0100

    modif heroku postbuild

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'reactapp/build')));
+app.use(express.static(path.join(__dirname, '/reactapp/build')));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname,'/reactapp/build/index.html'));
 });
